048b5f4285fde07746c187d5f5391176d911f988
Anmol kumar <cypher0017@gmail.com>
2025-01-25 11:34:01 +0530

    program

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
 print("Hello world")
+
+print("Enter Details:")
+name = input()
+age = input()
+roll = input()
+
+myList =[]
+myList.append(name)
+myList.append(age)
+myList.append(roll)
+
+print(myList)
---
+++ palindrom.py
@@ -0,0 +1,9 @@
+mylist = [1,2,3,2,1]
+
+mylist2 = mylist.copy()
+mylist2.reverse()
+
+if (mylist == mylist2):
+    print("palindrome")
+else:
+    print("not palindrome")    
